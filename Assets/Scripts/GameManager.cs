1a8f12c7fc7382be92ea8046fda5053dc9ce8ba7
Yoshio-0 <90753067+Yoshio-0@users.noreply.github.com>
2022-03-01 08:55:53 +0200

    tentative code for control scheme
    Collect resource
    Throw resource

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject rat;
+    [SerializeField] GameObject house;
+    [SerializeField] GameObject clickedGameObject;
+    [SerializeField] Text ratCounterText;
+
+    [SerializeField] int ratCounter = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rat = GameObject.Find("Rat");
+        house = GameObject.Find("House");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        KeyControle();
+    }
+
+    void KeyControle() 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            if (clickedGameObject == rat)
+            {
+                CollectRat();
+            }
+            else if (clickedGameObject == house)
+            {
+                if (Input.GetKey(KeyCode.Alpha1))
+                {
+                    ThrowRat();
+                }
+            }
+        }
+    }
+
+    void CollectRat() 
+    {
+        ratCounter++;
+        ratCounterText.text = "Rat Counter: " + ratCounter;
+    }
+
+    void ThrowRat() 
+    {
+        if (ratCounter > 0) 
+        {
+            ratCounter--;
+        }
+        ratCounterText.text = "Rat Counter: " + ratCounter;
     }
 }
